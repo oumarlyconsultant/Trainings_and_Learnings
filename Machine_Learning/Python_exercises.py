48da305528c0866d071731298118cdbc0cf6f17f
Oumar <oumarly.consultant@gmail.com>
2022-10-17 21:14:46 -0400

    update py exercises

--- Machine_Learning/Python_exercises.py
+++ Machine_Learning/Python_exercises.py
@@ -1,3 +1,4 @@
+####################################### LAMBDA #######################################
 # 1. Write a Python program to create a lambda function that adds 15 to a given number passed in as an argument, also create a lambda function that multiplies argument x with argument y and print the result. Go to the editor
 # Sample Output:
 # 25
@@ -63,3 +64,61 @@ f_same(s,'m')
 
 check_if_numb = lambda n: True if (n.startswith('-') and n[1:].isdigit()) or n.isdigit() == 'int' else False  
 check_if_numb('-778')
+
+
+
+###################################### DATETIME #####################################################
+import time
+import datetime as dt
+
+current = dt.datetime.now()
+
+print(current)
+type(current)
+
+
+############################## PANDAS ###########################################
+
+
+############################## NUMPY ############################################
+
+
+
+############################## SKLEARN ##########################################
+
+
+
+
+############################## CHALLENGE ########################################
+# 1. Write a Python program to check if a given positive integer is a power of two.
+import numpy as np
+
+pow2 = lambda x: True if (np.log(x) / np.log(2)).is_integer() else False
+
+pow3 = lambda x: True if (np.log(x) / np.log(3)).is_integer() else False
+
+
+pow2(36)
+pow3(21)
+#alternative
+def is_Power_of_two(n):
+        return n > 0 and (n & (n - 1)) == 0
+def is_Power_of_three(n):
+    while (n % 3 == 0):
+         n /= 3;         
+    return n == 1
+
+# 4. Write a Python program to check if a number is a perfect square. Go to the editor
+def is_perfect_square(n):
+    x = n // 2
+    y = set([x])
+    while x * x != n:
+        x = (x + (n // x)) // 2
+        if x in y: return False
+        y.add(x)
+    return True
+
+#  14. Write a Python program to find the single element appears once in a list where every element appears four times except for one. 
+s = set[2,5,69,777]
+
+s
